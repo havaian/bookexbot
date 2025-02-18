fa070320dedb7a2a950f0d850a16c4b785064a80
havaian <m.usman.work@gmail.com>
2025-02-18 12:00:40 +0100

    Fixed handleBrowseAction from browse command not properly imported

--- src/handlers/callback_query.js
+++ src/handlers/callback_query.js
@@ -1,10 +1,10 @@
-import { handleBrowse } from "../commands/browse.js";
+import { handleBrowseAction } from "../commands/browse.js";
 
 // Handle callback queries separately
 export const handleCallbackQuery = async (ctx) => {
   try {
     if (ctx.callbackQuery) {
-      await handleBrowse.handleBrowseAction(ctx);
+      await handleBrowseAction(ctx);
     }
   } catch (error) {
     global.app.logger.error(error);
